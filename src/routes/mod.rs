fa2235c829505ea0ab5ee199ad6247c7ca262e06
Kuba Jaroszewski <jakub.jaroszewski@gmail.com>
2023-09-05 17:26:25 +0200

    Fix lint

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,6 +1,6 @@
-mod health_check;
+mod internal;
 mod subscriptions;
 mod subscriptions_confirm;
-pub use health_check::*;
+pub use internal::*;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
